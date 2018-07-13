d56cf26c3fd97677ce8687078c8660ed4f6249da
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-13 19:00:22 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # person.rb
+
+class Person
+  
+  attr_accessor :adele_goldberg, :alan_kay
+  
+def initialize(adele_goldberg,alan_kay)
+  @adele_goldberg = adele_goldberg
+  @alan_kay = alan_kay
+  end 
+end 
+
+person_name = Person.new("adele_goldberg","alan_kay")
+
+puts person_name.adele_goldberg
+puts person_name.alan_kay
+
+
+
+  
+  
